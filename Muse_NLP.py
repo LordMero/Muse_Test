61378658dcd7b285b1487d4a17192a22e9b554ae
Graz <graziano.mirata@gmail.com>
2016-03-06 18:18:23 +0000

    New Update From Mac Mini

--- Muse_NLP.py
+++ Muse_NLP.py
@@ -1,10 +1,11 @@
 
-import pdb
+import ipdb
 
-# import minimum modules
+# ======================================================================
+#                        import modules
+# ======================================================================
 import numpy as np
 import sys
-import time
 import getopt
 
 # import functions and data structs
@@ -12,21 +13,31 @@ from math import log, exp
 from collections import defaultdict
 
 
-def ngrams_create(inp_str, n=3):
+
+
+
+
+
+
+# ======================================================================
+#                        Define Main Functions
+# ======================================================================
+
+def ngrams_create(inp_str, n):
     # this function create ngrams for each string  passed in inp_str
 
         # we want to enhance the strin with start and stop char
+
         inp_str_enh = (n-1) * ["*"]
-        inp_str_enh.extend(inp_str)
+        inp_str_enh.extend([items for items in inp_str])
         inp_str_enh.append("#")
-        ngrams = np.asarray([tuple(inp_str_enh[i:i+n]) for i in xrange(len(inp_str_enh)-n+1)])
+
+        ngrams = ([tuple(inp_str_enh[i:i+n]) for i in xrange(len(inp_str_enh)-n+1)])
         # return np array
-        return ngrams
+        return np.asarray(ngrams)
 
 
-
-
-def corpus_parser(corpus_file, comment_str=";;;",dic_flag=None):
+def corpus_parser(corpus_file, comment_str,sent_splitter, dic_flag):
 
     # this function is ment to parse the file and return words or sentences in a clean format
 
@@ -36,7 +47,6 @@ def corpus_parser(corpus_file, comment_str=";;;",dic_flag=None):
     com_char = len(comment_str)
     # inizialize output
     out = []
-
     # read corpus line by line
     l = corpus_file.readline()
     while l:
@@ -44,8 +54,16 @@ def corpus_parser(corpus_file, comment_str=";;;",dic_flag=None):
         # check - out for comments and skip those lines
         if (line[0:com_char] !=comment_str) and line:
 
-            fields = line.lower().split(" ",line.count(line) if dic_flag is None else 1)
-            #fields.remove("")
+            if  dic_flag is None:
+                # if I have text type trayining file
+                fields = line.lower().split(sent_splitter)[0]
+                fields = [eol+" " for eol in fields.split(" ") if eol !=""]
+
+            else:
+                # if I have a dictionary style training file I don't want other symbols in my training sample.
+                fool    = line.lower().split(" ",1)
+                fields  = list(fool[0])
+
             # building the list to be converted in np array
             out.append(fields)
             # move to the next line
@@ -53,11 +71,11 @@ def corpus_parser(corpus_file, comment_str=";;;",dic_flag=None):
         else:
             l = corpus_file.readline()
             # return np array
+
     return np.asarray(out)
 
 
-
-def ngrams_counter(input_array, n=3):
+def ngrams_counter(input_array, n):
     # this function counts the ngrams available in the entire corpus!
 
     # input array is of dimention 1 containing words or sentences (aka ws)
@@ -67,18 +85,7 @@ def ngrams_counter(input_array, n=3):
     ngram_counts = [defaultdict(int) for i in xrange(n)]
     # loop through each ws
 
-    # setup toolbar
-    sys.stdout.write("[Training: %s]" % (" " * input_array.size))
-    sys.stdout.flush()
-    sys.stdout.write("\b" * (input_array.size+1)) # return to start of line, after '['
-
-
     for words in input_array:
-
-        #time.sleep(0.1) # do real work here
-        # update the bar
-        sys.stdout.write("=")
-        sys.stdout.flush()
 
         # create ngrams for each ws
         ngrams_array = ngrams_create(words,n)
@@ -95,34 +102,47 @@ def ngrams_counter(input_array, n=3):
 
     return ngram_counts
 
-def estimate_probs(input_str, ngram_counts, n=3):
+def estimate_probs(tinput_str, ngram_counts, n):
     # this function estimates the ngrams conditional probabilities approximated with frequencies contained in ngram_counts
 
-    # make sting comparable
-    input_str = input_str.lower()
+    # make sting comparable and put into a list as ngrams_create is expecting that
+
+    tinput_str = tinput_str.lower()
+    print tinput_str
+    # need to preserve spaces
+    input_str  = [eol+" " for eol in tinput_str.split(" ") if eol !=""]
     # first get the number of unique letters or word in corpus
     tot_unique = sum(ngram_counts[0].values())
     # this function is expecting a list with n default dictionaries
-    ngram_array = ngrams_create(input_str)
+    ngram_array = ngrams_create(input_str,n)
 
     # alloc conditional probabilities and weights, firs col ws, second col cond prob third col lambdas
     # note that this can be further improved by computing ex ante the maximum size for words
+
+    #pdb.set_trace()
     cond_prob = np.array([(item, 1, 1) for item in ngram_array[:,-1]], \
                           dtype=[('ws','S50'),('logprob','f4'),('lambda','f4')])
 
     # set up a counter
+
     i = 0
     for ngram in ngram_array:
-        # if my ngram is not in the hash table I'll handle the exception by giving zero to the event - this must be address by smoothing the probabilities i.e. evaluate the model, reduce other count and increase the missing ones
+        # if my ngram is not in the hash table I'll handle the exception by giving zero to the event - this must be address by smoothing the probabilities i.e. evaluate the model, reduce other counts and increase the missing ones
+
         try:
             if ngram[n-2] == '*':
                 num = ngram_counts[0][''.join(ngram[-1])]
                 den = tot_unique
+
             else:
                 num = ngram_counts[n-1][''.join(ngram[:])]
                 den = ngram_counts[n-2][''.join(ngram[-n:-1])]
+                ipdb.set_trace()
+                print num, den
+
             # store probabilities in log format
             cond_prob['logprob'][i] = log(float(num)/den)
+
         except:
             cond_prob['logprob'][i] = log(10**-20)
 
@@ -132,98 +152,165 @@ def estimate_probs(input_str, ngram_counts, n=3):
 
     return input_str,exp(word_prob),cond_prob
 
-def query_system(tquery_str,ngram_counts,top,n=3):
+def query_system(tquery_str,ngram_counts,top,n):
 
-    # this function query the system to guess the top x possibility
+    # this function query the system to guess the top x possibilities
 
-    # defining query symbols
-    q_str  = '?'
-    q_word = '?*'
 
     # handle request
     query_str = np.chararray(1,len(tquery_str))
     query_str[:] = tquery_str
 
     # get index of missing ws
-    inx = query_str.rindex(q_str)[0]
+    try:
+        inx = query_str.rindex('*')[0]
+        wcard = '?*'
+    except ValueError:
+        print "Wrong wildcard looking for the alternative"
+        try:
+            inx = query_str.rindex('?')[0]
+            wcard = '?'
+        except ValueError:
+            print "Expecting '?' or '?*' as wildcards."
+            sys.exit(2)
 
     # first get monograms
     monograms = ngram_counts[0];
 
-    # allocate output
-    guess_probs = np.array([(query_str.replace(q_str,mono)[0], 0) for mono in monograms.keys()],dtype=[('guessed_word','S50'),('guessed_probs','f4')])
+    # allocate arrays
+    guess_probs = np.array([(query_str.replace(wcard,mono)[0], 0) for mono in monograms.keys()],dtype=[('guessed_word','S50'),('guessed_probs','f4')])
+    out         = np.empty([1,top],dtype=str)
 
     # compute probabilities
     for i in xrange(0,len(guess_probs['guessed_word'])):
         guess_probs['guessed_probs'][i] = estimate_probs(guess_probs['guessed_word'][i],ngram_counts,n)[1]
 
     # hold on to positive probabilities
-    guess_probs = guess_probs[guess_probs['guessed_probs']>0]
+    guess_probs = guess_probs[guess_probs['guessed_probs']>10**-10]
 
     if guess_probs.size:
         # sort for the most likely
         guess_probs = np.sort(guess_probs,order='guessed_probs')
 
         for i  in xrange(1,top+1):
-            return guess_probs[-i]
+            print guess_probs[-i]
+            #out[0,i-1] = guess_probs[-i][0][inx]
+        #return out
     else:
         return None
 
 
-def main(argv):
 
-    main_inputs = parse_options(argv)
 
-    if len(main_inputs[0]) == 0:
-        input_ws = raw_input("Please insert word to predict: ")
 
-    pdb.set_trace()
 
-    input_array = (corpus_parser(open(main_inputs[2],'r')))
+# ======================================================================
+#              Define Utility & Windon Dressing Functions
+# ======================================================================
 
-    count_array = ngrams_counter(input_array[:,0])
-
-    out = query_system(input_ws,count_array,main_inputs[1])
-
-    print  out
 
 def usage():
-    print 'how to use'
+    print """
+    Muse NLP usage: Muse_NLP.py [--version] [--help] [-i | -input <string>]
+                                [-t | --top int val] [-f | --file <path>]
+                                [-n | --ngrams int val]
 
-def parse_options(argv):
-    try:
-        opts, args = getopt.getopt(argv,"hi:t:f:n:",["input=","top=","file=","ngrams="])
-    except:
-        usage()
-        sys.exit(2)
+        This script uses ngrams as Natural Language Processing model to guess missing letters or words.
+        Please use '?' or '?*' respectively to indicate the expecting letter or word (i.e. 'Hel?o' of 'Hello ?*').
+        Arguments:
 
-    if len(opts) != 0:
+            -i not optional, input string: -i 'Hel?o'
+            -t optional number of top guesses, defualt: -t 1
+            -f path to training file
+            -n optional number of ngrams to be calculated, default: -n 3
+    """
+def version():
+    print """
+        Muse NLP for python 2.x, version 1.0.0
+        Written by Graziano Mirata (graziano.mirata@gmail.com)
+    """
+
+
+class parse_inputs(object):
+    """
+    Parses inputs from command line
+    """
+
+    def __init__(self):
+            # set defaults
+        self.top         = 1
+        self.input_file  = 'cmudict-0.7b.txt'
+        self.n           = 3
+        self.dic_flag    = None
+        self.split       = None
+        self.comment_str =";;;"
+        self.input_str   = ''
+
+    def parse_options(self, argv):
+
+        self.input_str = argv[0]
+        try:
+            opts, args = getopt.getopt(argv[1:],"hvs:t:f:d:n:c:",\
+            ["help","version","top=","dfile=","file=","split=","comm=","ngrams="])
+        except getopt.GetoptError:
+            usage()
+            sys.exit(2)
+
         for opt, arg in opts:
-            if opt == '-h':
+            if opt in ("-h","--help"):
                 usage()
                 sys.exit()
-            elif opt in ("-i", "--input"):
-                 input_ws = arg
+            elif opt in ("-v","--version"):
+                version()
+                sys.exit()
             elif opt in ("-t", "--top"):
-                 top = arg
+                self.top = int(arg)
+            elif opt in ("-d","--dfile"):
+                self.input_file = arg
+                self.dic_flag   = 1
             elif opt in ("-f","--file"):
-                input_file = arg
+                self.input_file = arg
+                self.dic_flag   = None
+            elif opt in ("-s","--split"):
+                self.split = arg
+            elif opt in ("-c","--comm"):
+                self.comment_str = arg
             elif opt in ("-n","--ngrams"):
-                n = arg
-    else:
-        input_ws = ""
-        top = 1
-        input_file = 'cmudict-0.7b.txt'
-        n = 3
+                self.n = int(arg)
 
     # if input_file is not provided use the default one. It's assumed to be in the same directory where the script is
-    return input_ws,top,input_file,n
+    #return input_ws,top,input_file,n,dic_flag,split,comment_str
+
+
+
+
+
+
+
+
+
+# ======================================================================
+#                        MAIN
+# ======================================================================
+
+
+def main(argv):
+
+    i_parser = parse_inputs()
+
+
+    i_parser.parse_options(argv)
+
+    #main_inputs = parse_options(argv)
+
+    input_array = corpus_parser(open(i_parser.input_file,'r'),i_parser.comment_str,i_parser.split, i_parser.dic_flag)
+
+    count_array = ngrams_counter(input_array,i_parser.n)
+
+    out = query_system(i_parser.input_str,count_array,i_parser.top,i_parser.n)
+
+    print  out
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-
-
-
